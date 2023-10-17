0d413c6f79c987b3bbac3a6ed3b5cb0e70b7aa10
Connor <Kon_Priest@Hotmail.com>
2023-10-17 18:12:51 +0100

    Added Unreal 4.27.2 project

---
+++ Source/GridTD/GridTDGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GridTDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIDTD_API AGridTDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
