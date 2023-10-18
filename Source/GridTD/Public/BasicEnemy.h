496a6f6c0901e8d8a79b77cec535445b38482667
Connor <Kon_Priest@Hotmail.com>
2023-10-18 16:43:09 +0100

    Created a base for enemies

---
+++ Source/GridTD/Public/BasicEnemy.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BasicEnemy.generated.h"
+
+class UCapsuleComponent;
+
+UCLASS()
+class GRIDTD_API ABasicEnemy : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABasicEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Compomnents")
+	UCapsuleComponent* CapsuleComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components");
+	UStaticMeshComponent* StaticMeshComponent;
+};
