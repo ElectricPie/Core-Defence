1dd38a5c8722f5fe2122067b73bab3ece8211ad8
Connor <Kon_Priest@Hotmail.com>
2023-12-13 01:11:48 +0000

    Created Layout for Player Resources a Health
    Health orb grid has scale -1 to have it aligned to the right

---
+++ Source/GridTD/Public/Ui/PlayerResourceWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PlayerResourceWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIDTD_API UPlayerResourceWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
